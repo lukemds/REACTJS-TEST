0aa6f9847120534f428bc82f13ae2321dce439df
Lucas <lukemdsousa@gmail.com>
2021-12-15 11:33:04 -0300

    feat: svgs

---
+++ src/svgs/index.jsx
@@ -0,0 +1,5 @@
+import { ReactComponent as ArrowDown } from "./arrowdown/arrowdown.svg";
+import { ReactComponent as Pokeball } from "./pokeball/pokeball.svg";
+import { ReactComponent as PokeballBackground } from "./pokeball/pokeball_background.svg";
+
+export { ArrowDown, Pokeball, PokeballBackground };
