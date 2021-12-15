115823c450fb40c987eabf2ca2d6b4904d6a181f
Lucas <lukemdsousa@gmail.com>
2021-12-15 11:18:11 -0300

    feat: configurações iniciais

---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" />
+        <Route path="/:id" />
+      </Routes>
+    </>
+  );
+}
